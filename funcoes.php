2fba2b141c63765b9f636a169e48339f705f8e17
MARCELO HENRIQUE XAVIER <marcelo.hxavier@senacsp.edu.br>
2024-10-21 21:23:33 -0300

    Segundo Commit

--- funcoes.php
+++ funcoes.php
@@ -19,7 +19,7 @@
             return $num1 / $num2;
         }else{
             return "Impossivel dividir por zero!"; 
-        }
+        }//Fim do if
     }//Fim do metodo
 
     function mostrarNumeros($num1,$num2){
@@ -37,30 +37,142 @@
             return "\nO número".$num1. " é negativo!";
         }else{
             return "\nO número".$num1. " é zero!";
-        }
-    }
+        }//Fim do if
+    }//Fim do metodo
 
     function tabuada($num1){
         $resultado = "";
         for($i=0;$i<=10;$i++){
             $resultado .= "\n".$num1."*".$i."=".($num1*$i);
-        }
+        }//Fim do for
         return $resultado;
-    }
+    }//Fim do metodo
 
     function fatorial($num1){
         $fatorial = 1;
         while ($num1 > 1){
             $fatorial *= $num1;
             $num1 -= 1;
-        }
+        }//Fim do while
         return "\nO fatorial do número digitado é ".$fatorial;
-    }
+    }//Fim do metodo
 
-    function raizPotencia($num1){
-        return Math.Pow($num1);//potencia
-        return Math.sqrt($num1);//raiz
-    }
+	function potencia($num1, $num2){
+        return "\nA potencia de ".$num1." é ". (pow($num1, $num2));//potencia  
+    }//Fim do metodo
 
+    function raiz($num1){
+        return "\nA raiz de ".$num1." é ". (sqrt($num1));//raiz
+    }//Fim do metodo
+
+	function parImpar($num1){
+        if ($num1 % 2 == 0){
+            return "\nO numero digitado é par";
+        }else{
+            return "\nO numero digitado é impar";
+        }//Fim do if
+    }//Fim do metodo
     
+    function ordemCrescente($num1,$num2){
+        if($num1>$num2){
+            return "\nO primeiro numero é maior que o segundo numero";
+        }else{
+            return "\nO segundo numero é maior que o primeiro numero";
+        }//Fim do if
+    }//Fim do metodo
+
+    function mediaNotas($num1, $num2, $num3){
+        $media = 0;
+        $media = ($num1 + $num2 + $num3) / 3;
+        return "\nA media das notas é de: ".$media ; 
+        
+    }//Fim do metodo
+
+    function vogais($str){
+        $cont = strlen($str);//Conta quantas caracteres tem dentro da string
+        $contar = 0;
+        $verificar = "";
+        for($i=0;$i<$cont;$i++){
+            $verificar = substr($str, $i, 1);//substr(navega por dentro da string)
+            if(($verificar == 'a') || ($verificar == 'e') || ($verificar == 'i') || ($verificar == 'o') ||($verificar == 'u')){
+                $contar++;
+            }
+        }//Fim do for
+        return "\nExistem ".$contar." vogais na palavra!";
+    }//Fim do metodo
+
+    function inverterString(string $texto){
+        return strrev($texto);//Biblioteca que inverte o texto
+    }//Fim do metodo
+
+    function primo($num1){
+        if (($num1 % 2 == 0) || ($num1 % 3 == 0) || ($num1 % 5 == 0)){
+            return "\nO número não é primo";
+        }else if(($num1 == 2) || ($num1 == 3) || ($num1 == 0)){
+            return "\nO número é primo";
+        }else{
+            return "\nO número é primo";
+        }//Fim do if
+    }//Fim do metodo
+
+    function exercicioUm($num1,$num2){
+        return "\nO valor de ".$num1."+".$num2."*".$num1." é: ".(($num1 + $num2) * $num1);
+    }//Fim do metodo
+
+    function exercicioTres($num1){
+        return "\n15% de ".$num1." é ".(($num1 * 15) / 100);
+    }//Fim do metodo
+
+    function exercicioQuatro($num1){
+        return "\n5% de ".$num1." é ".(($num1 * 5) / 100)."\n50% de ".$num1." é ".(($num1 * 50) / 100);
+    }//Fim do metodo
+
+    function exercicioCinco($num1,$num2){
+        return "\nO quadrado de ".$num1." é ".($num1*$num1)."\nO quadrado de ".$num2." é ".($num2*$num2);
+    }//Fim do metodo
+
+    function exercicioSeis($num1){
+        return "\nA velocidade ".$num1."m/s em Km/h é ". ($num1 * 3.6)."Km/h"; 
+    }//Fim do metodo
+
+    function exercicioSete($peso,$altura){
+        return "\nO IMC com base na altura e peso fornecido é de: ". (($altura*$altura)/$peso);
+    }//Fim do metodo
+
+    function exercicioOito($num1){
+        return "\nCom o desconto de 9% o valor de ".$num1." foi para ".((($num1 * 9) / 100) - $num1);
+    }//Fim do metodo
+
+    function exercicioNove($num1){
+        return "\nCom o desconto de 7% o valor de ".$num1." foi para ".((($num1 * 7) / 100) - $num1);
+    }//Fim do metodo
+
+    function exercicioDez($altura,$comprimento,$largura){
+        return "\nO volume da caixa retangular é de: ". ($comprimento * $largura * $altura);
+    }//Fim do metodo
+
+    function exercicioOnze($num1){
+        return "\n27% de ".$num1." é ".(($num1 * 27) / 100);
+    }//Fim do metodo
+
+    function exercicioDoze($base,$altura){
+        return "\nA area do trapezio é: ". ((($base * 2) * $altura)/ 2);
+    }//Fim do metodo
+
+    function exercicioTreze($nota1,$nota2, $nota3){
+        $peso2 = $nota1 * 2;
+        $peso3 = $nota2 * 3;
+        $peso5 = $nota3 * 5;
+        return "\nA media ponderada das 3 notas é: ". (($peso2 + $peso3 + $peso5)/3);
+    }//Fim do metodo
+
+    function exercicioQuatorze($num1){
+        $valorPorcentagem  = (($num1 * 27) / 100) + $num1;
+        $valorParcelado = $valorPorcentagem / 10;
+        return "\nO Valor total foi de R$".$valorPorcentagem." e o valor parcelado em 10x é de: R$".$valorParcelado;
+    }//Fim do metodo
+
+    function exercicioQuinze($km, $litros){
+        return "\nO consumo medio do veiculo foi de: ". $km / $litros;
+    }//Fim do metodo
 ?>
--- index.php
+++ index.php
@@ -15,10 +15,18 @@
         <label>Segundo Número: </label>
         <input type="text" name="num2" id="num2"/><br><br>
 
+        <label>Terceiro Número: </label>
+        <input type="text" name="num3" id="num3"/><br><br>
+
+        <label>Palavra: </label>
+        <input type="text" name="str" id="str"/><br><br>
+
         <button>Calcular
             <?php
                 $num1 = $_POST['num1'];//Coletar os dados
                 $num2 = $_POST['num2'];//Coletar os dados
+                $num3 = $_POST['num3'];//Coletar os dados
+                $str = $_POST['str'];//Coletar os dados
             ?>
         </button><br><br>
 
@@ -32,10 +40,31 @@
                 echo verificarNumero($num1);
                 echo tabuada($num1);
                 echo fatorial($num1);
-                echo raizPotencia($num1);
-
+                echo potencia($num1, $num2);
+                echo raiz($num1);
+                echo parImpar($num1);
+                echo ordemCrescente($num1,$num2);
+                echo mediaNotas($num1,$num2,$num3);
+                echo vogais($str);
+                echo inverterString($str);
+                echo primo($num1);
+                echo exercicioUm($num1,$num2);
+                echo exerciciotres($num1);
+                echo exercicioQuatro($num1);
+                echo exercicioCinco($num1, $num2);
+                echo exercicioSeis($num1);
+                echo exercicioSete($num1, $num2);
+                echo exercicioOito($num1);
+                echo exercicioNove($num1);
+                echo exercicioDez($num1,$num2,$num3);
+                echo exercicioOnze($num1);
+                echo exercicioDoze($num1,$num2);
+                echo exercicioTreze($num1,$num2,$num3);
+                echo exercicioQuatorze($num1);
+                echo exercicioQuinze($num1,$num2);
             ?>
         </textarea>
     </form>
+
 </body>
 </html>
